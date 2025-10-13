d56800cd78689f6e07842a2cc9cd1ef38db3080f
HaPpY <930333105@qq.com>
2025-10-13 10:13:09 +0800

    feat(model): 实现LeNet模型的前向传播逻辑- 添加了forward方法定义数据流经网络层的过程 - 在主函数中增加了模型实例化和结构打印功能 - 引入了设备检测逻辑以支持CUDA加速 - 使用summary工具展示模型架构细节

--- LeNet/model.py
+++ LeNet/model.py
@@ -15,3 +15,20 @@ class LeNet(nn.Module):
         self.f5 = nn.Linear(400, 120)
         self.f6 = nn.Linear(120, 84)
         self.f7 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.sig(self.c1(x))
+        x = self.s2(x)
+        x = self.sig(self.c3(x))
+        x = self.s4(x)
+        x = self.flatten(x)
+        x = self.f5(x)
+        x = self.f6(x)
+        x = self.f7(x)
+        return x
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # print(device)
+    model = LeNet().to(device)
+    print(summary(model, (1, 28, 28)))
